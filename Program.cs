e9adc4513508465499ea11d91787ad794a33e373
Patricia Takehara Borges <102318922+Takepaty@users.noreply.github.com>
2022-12-08 12:00:18 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,19 +8,22 @@
       var produto = 1;
       var soma = 0;
       var resultado = 0;
+      
       Console.WriteLine("Informe 3 números: ");
       numeroDigitado = Console.ReadLine();
+      
       foreach (var caracter in numeroDigitado.ToCharArray())
       {
         var numero = Convert.ToInt32(caracter.ToString());
         produto = produto * numero;
         soma = soma + numero;
       }
+      
       resultado = produto - soma;
+      
       // Console.WriteLine(soma);
       // Console.WriteLine(produto);
       Console.WriteLine(resultado);
-
     }
   }
 }
